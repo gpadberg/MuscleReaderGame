e6bbbf41c87fee2e51ad5b318f7b4e9b699724de
Grace Padberg <gracepadberg@Graces-MacBook-Pro.local>
2023-11-17 16:52:40 -0700

    pages added

--- phobiatesterwebapp/src/App.js
+++ phobiatesterwebapp/src/App.js
@@ -4,12 +4,17 @@ import FrontPage from './components/FrontPage';
 import * as React from "react"
 import StartPage from './components/StartPage';
 import TestInterface from './components/TestInterface';
+import ResultsPage from './components/ResultsPage';
+import WhatsNextPage from './components/WhatsNextPage';
+import PhobiaDefinitions from './components/PhobiaDefinitions';
+
+
 
 function App() {
 
   return (
     <div className="App">
-      <TestInterface/>
+      <PhobiaDefinitions/>
     </div>
   );
 }
