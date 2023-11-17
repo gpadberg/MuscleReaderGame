77a9d0ce066e0c3de1d4e96ea9dad74ed61a4b15
Grace Padberg <gracepadberg@Graces-MacBook-Pro.local>
2023-11-17 16:57:38 -0700

    pages added

--- phobiatesterwebapp/src/components/PhobiaDefinitions.js
+++ phobiatesterwebapp/src/components/PhobiaDefinitions.js
@@ -2,9 +2,10 @@ import * as React from "react"
 
 
 
-export default function StartPage(props) {
+export default function ResultsPage(props) {
     return(<div class="bg-black text-white min-h-screen">
     <div class="flex justify-between w-full px-96 pt-8">
+    <a href="#" class="text-white mb-2">Home</a>
       <a href="#" class="text-white mb-2">Phobia Definitions</a>
       <a href="#" class="text-white mb-2">About Us</a>
       <a href="#" class="text-white mb-2">What's Next?</a>
@@ -13,8 +14,15 @@ export default function StartPage(props) {
       <div class="flex justify-center items-center space-x-5 mb-8">
         <img alt="Logo" src="/phobiatestlogo.png" class="w-[30rem] h-[20rem] mb-6"></img>
       </div>
-      <h1 class="text-5xl font-bold mb-14">Phobia Definitions</h1>
-      <p class="text-xl mr-6 ml-6 mb-14">PhobiaTester gives you a conclusive reading of your reactivity to common phobias. Learn more about yourself through our interactive analysis.</p>
+      <h1 class="text-5xl font-bold mb-14">Your Results</h1>
+      <ul class="text-xl mr-6 ml-6 mb-2">
+        <li>placehold</li>
+        <li>placehold</li>
+        <li>placehold</li>
+        <li>placehold</li>
+        <li>placehold</li>
+        <li>placehold</li>
+        </ul>
     </div>
   </div>
     )
