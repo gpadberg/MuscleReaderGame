9e7a8ef1897110a0a56e1659a40a2f396c1a225d
James Stephen <scravers@Jamess-MacBook-Pro.local>
2023-11-17 11:45:43 -0700

    Slight change

--- phobiatesterwebapp/src/App.js
+++ phobiatesterwebapp/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header';
+import StartPage from './components/StartPage';
 import * as React from "react"
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <StartPage></StartPage>
     </div>
   );
 }
