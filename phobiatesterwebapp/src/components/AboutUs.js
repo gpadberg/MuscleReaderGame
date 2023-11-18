5a4acf029c52b11b4f158faa44458de4620354e6
Jacob Mellick <jacob@thenormaldose.com>
2023-11-18 14:09:04 -0700

    changed photo names to match phobias, migrated test into ImageChanger.js (the test doesn't happen in App anymore)

--- phobiatesterwebapp/src/components/AboutUs.js
+++ phobiatesterwebapp/src/components/AboutUs.js
@@ -1,21 +1,34 @@
-import * as React from "react"
-
-
+import * as React from "react";
 
 export default function StartPage(props) {
-    return(<div class="bg-black text-white min-h-screen">
-    <div class="flex justify-between w-full px-96 pt-8">
-      <a href="#" class="text-white mb-2">Home</a>
-      <a href="#" class="text-white mb-2">Phobia Definitions</a>
-      <a href="#" class="text-white mb-2">What's Next?</a>
-    </div>
-    <div class="text-center mt-28">
-      <div class="flex justify-center items-center space-x-5 mb-8">
-        <img alt="Logo" src="/phobiatestlogo.png" class="w-[30rem] h-[20rem] mb-6"></img>
+  return (
+    <div class="bg-black text-white min-h-screen">
+      <div class="flex justify-between w-full px-96 pt-8">
+        <a href="#" class="text-white mb-2">
+          Home
+        </a>
+        <a href="#" class="text-white mb-2">
+          Phobia Definitions
+        </a>
+        <a href="#" class="text-white mb-2">
+          What's Next?
+        </a>
       </div>
-      <h1 class="text-5xl font-bold mb-14">About Us</h1>
-      <p class="text-xl mr-14 ml-14 mb-14">We are a team of five students from the University of Alberta. We came together to create this project during the 2023 natHacks Hackathon with the idea of researching heart rate responses to stimuli.</p>
+      <div class="text-center mt-28">
+        <div class="flex justify-center items-center space-x-5 mb-8">
+          <img
+            alt="Logo"
+            src="/phobiatestlogo.png"
+            class="w-[30rem] h-[20rem] mb-6"
+          ></img>
+        </div>
+        <h1 class="text-5xl font-bold mb-14">About Us</h1>
+        <p class="text-xl mr-14 ml-14 mb-14">
+          We are a team of five students from the University of Alberta. We came
+          together to create this project during the 2023 natHacks Hackathon
+          with the idea of researching heart rate responses to stimuli.
+        </p>
+      </div>
     </div>
-  </div>
-    )
+  );
 }
