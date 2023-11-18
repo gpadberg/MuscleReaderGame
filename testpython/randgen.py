fb403cf6119ebb8e5f1b7557ccd43bad2f032ff5
James Stephen <scravers@Jamess-MacBook-Pro.local>
2023-11-18 16:36:33 -0700

    added changes

--- testpython/randgen.py
+++ testpython/randgen.py
@@ -1,4 +1,14 @@
-import random
+from sys import argv 
+from random import randint
+fileName = "output.txt"
 
-for i in range(10):
-    print(random.randint(0,10))
+if __name__ == "__main__":
+    argVal = argv[1]
+    if argVal == "reset":
+        with open(fileName, "w") as myFile:
+            pass # empties the file
+    elif argVal == "advance":
+        print("Start Advance")
+        with open(fileName, "a") as myFile:
+            intVal = randint(100, 150)
+            myFile.write(f"{intVal}\n")
