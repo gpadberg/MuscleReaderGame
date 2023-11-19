b06de27d668c6c79e8ae933874706023b1d8dad5
Jacob Mellick <jacob@thenormaldose.com>
2023-11-18 19:12:34 -0700

    finished test functionality

--- SerialRead/sampleScript.py
+++ SerialRead/sampleScript.py
@@ -1,14 +1,20 @@
-from sys import argv 
+from sys import argv
 from random import randint
+
 fileName = "output.txt"
+from calculateBPM import do_generateBPM
 
 if __name__ == "__main__":
     argVal = argv[1]
     if argVal == "reset":
         with open(fileName, "w") as myFile:
-            pass # empties the file
+            pass  # empties the file
     elif argVal == "advance":
-        print("start Advance")
+        # bpm = do_generateBPM()
         with open(fileName, "a") as myFile:
             intVal = randint(100, 150)
             myFile.write(f"{intVal}\n")
+
+    elif argVal == "display":
+        with open(fileName, "r") as myFile:
+            print(myFile.readlines())
